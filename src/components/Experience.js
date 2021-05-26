6ef702b42cb4a8d1c892fb35682cf3754767d181
amanpreetdotnet <amanpreets129@gmail.com>
2021-05-26 15:32:20 +1000

    experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div className="experience">
+        <div className="d-flex justify-content-center my-5">
+            <h1>experience</h1>
+        </div>
+        <div className="container experience-wrapper">
+            <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2014-2015</h3>
+                    <p>I'm the Web Developer for StrikeOutStroke in Melbourne. I have serious passion for coding, designing and creating intuitive, dynamic user experiences. I loves programming, writing, speaking and traveling. I am the founder of Websoles, Web Development Company</p>
+                </div>
+            </div>
+            {/*  */}
+            <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2014-2015</h3>
+                    <p>I'm the Web Developer for StrikeOutStroke in Melbourne. I have serious passion for coding, designing and creating intuitive, dynamic user experiences. I loves programming, writing, speaking and traveling. I am the founder of Websoles, Web Development Company</p>
+                </div>
+            </div>
+                {/*  */}
+                <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2015-2016</h3>
+                    <p>I'm the Web Developer for StrikeOutStroke in Melbourne. I have serious passion for coding, designing and creating intuitive, dynamic user experiences. I loves programming, writing, speaking and traveling. I am the founder of Websoles, Web Development Company</p>
+                </div>
+            </div>
+            {/*  */}
+            <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2016-2017</h3>
+                    <p>I'm the Web Developer for StrikeOutStroke in Melbourne. I have serious passion for coding, designing and creating intuitive, dynamic user experiences. I loves programming, writing, speaking and traveling. I am the founder of Websoles, Web Development Company</p>
+                </div>
+            </div>
+            <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2017-2018</h3>
+                    <p>I'm the Web Developer for StrikeOutStroke in Melbourne. I have serious passion for coding, designing and creating intuitive, dynamic user experiences. I loves programming, writing, speaking and traveling. I am the founder of Websoles, Web Development Company</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Experience
--- src/components/Services.js
+++ src/components/Services.js
@@ -9,7 +9,7 @@ const Services = () => {
             <h1 className="py-5">my services</h1>
                 <div className="container">
                     <div className="row">   
-                        <div className="col-md-3 col-sm-6">
+                        <div className="col-lg-3 col-md-6 col-sm-6">
                             <div className="box">
                             <div className="circle"><FontAwesomeIcon className="icon" icon={faDesktop} size="2x"/></div>
 
@@ -18,7 +18,7 @@ const Services = () => {
                             </div>
                         </div>
                         {/*  */}
-                        <div className="col-md-3 col-sm-6">
+                        <div className="col-lg-3 col-md-6 col-sm-6">
                             <div className="box">
                             <div className="circle"><FontAwesomeIcon className="icon" icon={faFacebookF} size="2x"/></div>
                                 <h3>Web Development</h3>
@@ -26,7 +26,7 @@ const Services = () => {
                             </div>
                         </div>
                         {/*  */}
-                        <div className="col-md-3 col-sm-6">
+                        <div className="col-lg-3 col-md-6 col-sm-6">
                             <div className="box">
                             <div className="circle"><FontAwesomeIcon className="icon" icon={faDesktop} size="2x"/></div>
                                 <h3>Social Media Markeing</h3>
@@ -34,7 +34,7 @@ const Services = () => {
                             </div>
                         </div>
                         {/*  */}
-                        <div className="col-md-3 col-sm-6">
+                        <div className="col-lg-3 col-md-6 col-sm-6">
                             <div className="box">
                             <div className="circle"><FontAwesomeIcon className="icon" icon={faGoogle} size="2x"/></div>
                                 <h3>Google Ads</h3>
